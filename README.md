2b1810662c38411e5400a34b73b349e001a54d13
Manh-Cuong Nguyen <goesjoomla@gmail.com>
2012-06-13 16:11:45 +0700

    Commit D4J Blue Leaves template to templates/blue_leaves branch.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-D4J Blue Blog template for Joomla! 1.0 series
+D4J Blue Leaves template for Joomla! 1.0 series
 ================================================
 
 Install Git client by following the guide at the URL below:
@@ -9,7 +9,7 @@ Then, clone the repository at following URL:
 
 https://github.com/goesjoomla/DesignForJoomla.com.git
 
-Finally, checkout templates/blue_blog branch to get D4J Blue Blog.
+Finally, checkout templates/blue_leaves branch to get D4J Blue Leaves.
 
 --
 Manh-Cuong Nguyen
--- installation/index.php
+++ installation/index.php
@@ -17,46 +17,40 @@ echo '<?xml version="1.0" encoding="'. $iso[1] .'"?' .'>';
 <link rel="stylesheet" type="text/css" href="<?php echo _TEMPLATE_URL; ?>/css/template_css.css" />
 <style type="text/css">
 <?php if (mosCountModules ('left') OR mosCountModules ('right')) { ?>
-          #lbox{width:410px}
-          #rbox{width:150px}
+          #lbox{width:175px}
+          #rbox{width:557px}
 <?php } else { ?>
-          #rbox{width:0px;height:0px}
-          #top,#bottom{width:565px}
-          #mainbody{width:550px}
-          #lbox{width:580px}
-          #footer_container,#content{background:none}
-          #footer{width:562px}
+          #rbox,#top,#bottom{margin:0;width:740px}
+          #top .moduletable,#bottom .moduletable{padding:0 10px 22px 10px}
+          #mainbody{margin:0;width:720px;padding:0 10px 22px 10px}
+          #lbox{width:0px}
 <?php } ?>
 </style>
 <!--[if lt IE 7]>
 <link rel="stylesheet" type="text/css" href="<?php echo _TEMPLATE_URL; ?>/css/template_css_ie.css" />
 <style type="text/css">
 <?php if (mosCountModules ('left') OR mosCountModules ('right')) { ?>
-          #lbox{width:410px}
-          #rbox{width:150px}
+          #lbox{width:175px}
+          #rbox{width:557px}
 <?php } else { ?>
-          #rbox{width:0px;height:0px}
-          #top,#bottom{width:565px}
-          #mainbody{width:580px}
-          #lbox{width:580px}
-          #footer_container,#content{background:none}
-          #footer_container{margin-top:-35px}
-          #footer{width:574px}
+          #rbox,#top,#bottom{margin:0;width:740px}
+          #top .moduletable,#bottom .moduletable{padding:0 10px 22px 10px}
+          #mainbody{margin:0;width:740px;padding:0 10px 22px 10px}
+          #lbox{width:0px}
 <?php } ?>
 </style>
-<![endif]-->
 
+<![endif]-->
 </head>
 <body><center>
 <div id="container">
-    <div id="header">
-               <div id="logan"><?php if (mosCountModules('user7')) mosLoadModules('user7', -2);
-                           else echo '<h1>Blue blog <b>template</b></h1>
-                                      <h2>Soft color blog template...</h2>'?>
-               </div>
-    </div>
-    <div id="subheader">
-    <div id="menu"><?php $database->setQuery("SELECT id,name,link,type,browserNav FROM #__menu WHERE menutype = 'mainmenu' AND published = 1 AND access <= $my->gid AND parent = 0 ORDER BY ordering LIMIT 0,7");
+        <div id="header">
+                 <div id="title"><?php if (mosCountModules('user7')) mosLoadModules('user7', -2);
+                           else echo '<h1>Company Name</h1><h2>to place for your slogan</h2>' ?>
+                 </div>
+        </div>
+
+        <div id="menu"><?php $database->setQuery("SELECT id,name,link,type,browserNav FROM #__menu WHERE menutype = 'mainmenu' AND published = 1 AND access <= $my->gid AND parent = 0 ORDER BY ordering LIMIT 0,6");
                     if ($rows = $database->loadObjectList()) {
                     echo'<ul>';
                     for ($i = 0, $n = count($rows); $i < $n; $i++) {
@@ -72,52 +66,24 @@ echo '<?xml version="1.0" encoding="'. $iso[1] .'"?' .'>';
 
                     }
                     echo '</ul>';} ?>
-    </div>
+        </div>
+        <div id="content">
+                <div id="lbox">
+                           <?php if (mosCountModules('left')) { ?><div id="left"><?php mosLoadModules('left', -2);?></div><?php } ?>
+                           <?php if (mosCountModules('right')) { ?><div id="right"><?php mosLoadModules('right', -2);?></div><?php } ?>
+                </div>
 
-    </div>
-    <div id="content">
-        <div id="lbox">
-                    <?php if (mosCountModules('top')) { ?><div id="top"><?php mosLoadModules('top', -2);?></div><?php } ?>
-                    <div id="mainbody"><?php mosMainbody() ?></div>
-                    <?php if (mosCountModules('bottom')) { ?><div id="bottom"><?php mosLoadModules('bottom', -2);?></div><?php } ?>
-                    <div class="clr"><!-- --></div>
+                <div id="rbox">
+                            <?php if (mosCountModules('top')) { ?><div id="top"><?php mosLoadModules('top', -2);?></div><?php } ?>
+                            <div id="mainbody"><?php mosMainbody() ?></div>
+                            <?php if (mosCountModules('bottom')) { ?><div id="bottom"><?php mosLoadModules('bottom', -2);?></div><?php } ?>
+                </div>
         </div>
 
-        <div id="rbox">
-               <div id="search_field"><?php $query = "SELECT id"
-                          . "\n FROM #__menu"
-                          . "\n WHERE link = 'index.php?option=com_search'";
-                          $database->setQuery( $query );
-                          $rows = $database->loadObjectList();
-                          // try to auto detect search component Itemid
-                          if ( count( $rows ) ) {
-                                  $_Itemid        = $rows[0]->id;
-                                  $link                 = 'index.php?option=com_search&amp;Itemid='. $_Itemid;
-                          } else {
-                          // Assign no Itemid
-                                  $_Itemid         = '';
-                                  $link                 = 'index.php?option=com_search';}?>
-                                            <form name="searchForm" action="<?php echo $link ?>" method="post" style="margin:0px">
-                                            <input type="hidden" name="option" value="com_search" />
-                                            <input type="hidden" name="Itemid" value="<?php echo $_Itemid ?>"/>
-                                               <div id="search1">
-                                                        <div id="inputbox">
-                                                                <input name="searchword" maxlength="40" alt="search" class="inputbox" type="text" value="<?php echo _SEARCH_TITLE; ?>" onfocus="if(this.value=='<?php echo _SEARCH_TITLE; ?>'){this.value='';}" onblur="if(this.value==''){this.value='<?php echo _SEARCH_TITLE; ?>';}"/>
-                                                        </div>
-                                                        <div id="button">
-                                                                <input class="button" type="image" alt="search" src="<?php echo _TEMPLATE_URL ?>/images/button.gif" onclick="document.searchForm.submit();" />
-                                                        </div>
-                                               </div>
-                                            </form>
-               </div>
-               <?php if (mosCountModules('left')) { ?><div id="left"><?php mosLoadModules('left', -2);?></div><?php } ?>
-               <?php if (mosCountModules('right')) { ?><div id="right"><?php mosLoadModules('right', -2);?></div><?php } ?>
-               <div class="clr"><!-- --></div>
-         </div>
-    </div>
-    <div id="footer_container">
-    <div id="footer"><?php if (mosCountModules('footer')) mosLoadModules('footer', -1); else include_once(_TEMPLATE_PATH.'/css/bottom.css.php'); ?></div>
-    </div>
+        <div class="clr"><!-- --></div>
+        <div id="footer_container">
+        <div id="footer"><?php if (mosCountModules('footer')) mosLoadModules('footer', -1); else include_once(_TEMPLATE_PATH.'/css/bottom.css.php'); ?></div>
+        </div>
 </div>
 </center><?php include_once(_TEMPLATE_PATH.'/css/footer.css.php') ?></body>
 </html><!-- Joomla Template by DesignForJoomla.com -->
