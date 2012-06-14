53e6e79dc57c44509edabfeef0fa775bdc731037
Manh-Cuong Nguyen <goesjoomla@gmail.com>
2012-06-14 14:01:23 +0700

    Commit D4J Fantasy 01 template to templates/fantasy_01 branch.

--- index.php
+++ index.php
@@ -1,59 +1,95 @@
-<?php /* Joomla Template by DesignForJoomla.com */
+<?php	/* Joomla Template by DesignForJoomla.com */
 
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
-define( '_TEMPLATE_URL', $mosConfig_live_site.'/templates/'.$cur_template );
-define( '_TEMPLATE_PATH', str_replace('\\', '/', dirname(__FILE__)) );
-
 $iso = split( '=', _ISO );
 echo '<?xml version="1.0" encoding="'. $iso[1] .'"?' .'>';
 
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<?php if ($my->id) initEditor() ?>
-	<?php mosShowHead() ?>
-	<meta http-equiv="Content-Type" content="text/html; <?php echo _ISO ?>" />
-	<link rel="stylesheet" type="text/css" href="<?php echo _TEMPLATE_URL ?>/css/template_css.css" />
-	<style type="text/css">
-	<?php if (mosCountModules('user1') AND mosCountModules('user2')) { ?>
-		#user1{float:left;width:50%}
-		#user2{float:none;margin-left:50%}
-		#user2 div.moduletable{padding-left:4px}
-	<?php } ?>
-	</style>
-	<!--[if IE]>
-	<link rel="stylesheet" type="text/css" href="<?php echo _TEMPLATE_URL ?>/css/template_css_ie.css" />
-	<![endif]-->
+	<?php if ($my->id) { initEditor(); } ?>
+	<?php mosShowHead(); ?>
+	<meta http-equiv="Content-Type" content="text/html; <?php echo _ISO; ?>">
+	<META NAME="revisit-after" CONTENT="1 days">
+	<meta name="Copyright" content="(c) Ju+Ju Group">
+	<meta name="Publisher" content="Your Mambo Design">
+	<meta name="Language" content="en">
+	<link rel="shortcut icon" href="<?php echo $GLOBALS['mosConfig_live_site'];?>/images/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['mosConfig_live_site']; ?>/templates/fantasy_01/css/template_css.css" />
 </head>
 
-<body><center>
-	<div id="farouter">
-		<div id="sleft">
-			<div id="logo">
-				<h1 title="<?php echo $mosConfig_sitename ?>"><a href="<?php echo $mosConfig_live_site ?>" title="<?php echo $mosConfig_sitename ?>"><?php echo $mosConfig_sitename ?></a></h1>
-			</div>
-			<?php if (mosCountModules ('left')) { ?><div id="mleft"><?php mosLoadModules('left', -2) ?></div><?php } ?>
-		</div>
+<body bgcolor="#000000" text="#000000" background="templates/fantasy_01/images/bg.jpg" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" link="#FFFFFF" vlink="#FFFFFF" alink="#FFFF33">
+<table width="777" border="0" cellspacing="0" cellpadding="4">
+  <tr> 
+    <td width="636" valign="top">
+    	<table width="100%" border="0" cellspacing="0" cellpadding="4">
+    		<tr>
+    			<td width="180" height="79" align="center" valign="middle">
+<!-- Show Company Logo - Begin -->
+	<a href="<?php echo $GLOBALS['mosConfig_live_site']; ?>" title="<?php echo strtoupper($GLOBALS['mosConfig_sitename']); ?>">
+		<img border="0" src="templates/fantasy_01/images/logo.gif" alt="<?php echo $GLOBALS['mosConfig_sitename']; ?> - <?php echo $mosConfig_MetaDesc; ?>" />
+	</a>
+<!-- Show Company Logo - End -->
+			    </td>
+    			<td width="456" valign="middle">
+<!-- Show Site's Title & Slogan - Begin -->
+<?php $site_title = explode(' - ', $GLOBALS['mosConfig_sitename']); ?>
+	<font face="Arial" size="3" color="#ff0000"><?php echo strtoupper($site_title[0]); ?></font><br/>
+	<font face="Arial" size="2" color="#ff6600"><?php echo $site_title[1]; ?></font>
+<!-- Show Site's Title & Slogan - End -->
+    			</td>
+    		</tr>
+    		<tr> 
+    			<td width="180" valign="top"> 
+<!-- Load Left Module - Begin -->
+	<?php 
+		mosLoadModules("left");
+	?>
+<!-- Load Left Module - End -->
+    			</td>
+    			<td width="456" align="justify" valign="top">
+<!-- Load User1, User2, Top Module, Main Content & Bottom Module - Begin -->
+	<?php if ( (mosCountModules('user1') > 0) OR (mosCountModules('user2') > 0) ) { ?>
+		<table width='100%' border='0' cellspacing='0' cellpadding='0'><tr>
+	<?php if ( (mosCountModules('user1') > 0) AND (mosCountModules('user2') > 0) ) { ?>
+		<td width='50%' align='center' valign='top'><?php mosLoadModules("user1"); ?></td>
+		<td width='50%' align='center' valign='top'><?php mosLoadModules("user2"); ?></td>
+	<?php } elseif (mosCountModules('user1') > 0) { ?>
+		<td width='100%' align='center' valign='top'><?php mosLoadModules("user1"); ?></td>
+	<?php } elseif (mosCountModules('user2') > 0) { ?>
+		<td width='100%' align='center' valign='top'><?php mosLoadModules("user2"); ?></td>
+	<?php } ?>
+		</tr></table>
+	<?php }
+		if (mosCountModules('top') > 0) { mosLoadModules ( "top" ); echo '<hr/>'; }
+		mosMainBody();
+		if (mosCountModules('bottom') > 0) { echo '<hr/>'; mosLoadModules ( "bottom" ); }
+	?>
+<!-- Load User1, User2, Top Module, Main Content & Bottom Module - End -->
+    			</td>
+    		</tr>
+    	</table>
+<!-- Credit Line - Begin -->
+<br/><br/><br/><br/><br/><br/><br/>
+<div align="center">
+Site template designed by <a href="http://www.petelove.com" target="_blank">PeteLove.com</a>.<a href="http://designforjoomla.com" target="_blank" title="Joomla template by DesignForJoomla.com">Joomla template by DesignForJoomla.com</a>.</div>
+<?php include_once( $GLOBALS['mosConfig_absolute_path'] . '/includes/footer.php' ); ?>
+</div>
+<!-- Credit Line - End -->
+    </td>
+    <td rowspan="2" width="141" align="justify" valign="top">
+<!-- Load Right Module - Begin -->
+	<?php 
+		if (mosCountModules('right') > 0) { mosLoadModules ( "right" ); }
+	?>
+<!-- Load Right Module - End -->
+    </td>
+  </tr>
+</table>
+</body>
+<!-- /* Joomla Template by DesignForJoomla.com */ -->
+</html>
 
-		<div id="container">
-			<div id="slogan"><?php if (mosCountModules('header')) mosLoadModules('header', -2); else {
-				echo '<div class="moduletable"><h3 title="'.$mosConfig_sitename.'">'.$mosConfig_sitename.'</h3>to <b>place</b> for <em>your slogan ...</em></div>';
-			} ?></div>
-			<div id="main">
-				<?php if (mosCountModules('user1')) { ?><div id="user1"><?php mosLoadModules('user1', -2) ?></div><?php } ?>
-				<?php if (mosCountModules('user2')) { ?><div id="user2"><?php mosLoadModules('user2', -2) ?></div><?php } ?>
-				<?php if (mosCountModules('user1') OR mosCountModules('user2')) { ?><br class="clr" /><?php } ?>
-				<?php mosMainbody() ?>
-    			<?php if (mosCountModules('bottom')) { ?><div id="bottom"><?php mosLoadModules('bottom', -2) ?></div><?php } ?>
-			</div>
-		</div>
 
-		<?php if (mosCountModules('right')) { ?><div id="sright"><div id="mright"><?php mosLoadModules('right', -2) ?></div></div><?php } ?>
-		<div class="clr"> <!-- --> </div>
-
-		<div id="footer"><?php if (mosCountModules('footer')) mosLoadModules('footer', -2); else include_once(_TEMPLATE_PATH.'/css/bottom.css.php'); ?></div>
-	</div>
-</center><?php include_once(_TEMPLATE_PATH.'/css/footer.css.php') ?></body>
-
-</html><!-- Joomla Template by DesignForJoomla.com -->
