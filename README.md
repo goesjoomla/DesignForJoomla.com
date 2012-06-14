bd2f89ba39e4c437a1b34865ec34e3613afc46e4
Manh-Cuong Nguyen <goesjoomla@gmail.com>
2012-06-14 13:57:53 +0700

    Commit D4J Fading Blue template to templates/fading_blue branch.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-D4J Extreme Updates template for Joomla! 1.0 series
+D4J Fading Blue template for Joomla! 1.0 series
 ===============================================
 
 Install Git client by following the guide at the URL below:
@@ -9,7 +9,7 @@ Then, clone the repository at following URL:
 
 https://github.com/goesjoomla/DesignForJoomla.com.git
 
-Finally, checkout templates/extreme_updates branch to get D4J Extreme Updates.
+Finally, checkout templates/fading_blue branch to get D4J Fading Blue.
 
 --
 Manh-Cuong Nguyen
--- installation/css/bottom.css.php
+++
@@ -1,9 +0,0 @@
-<div class="moduletable">
-         <div id="bottom_left">
-         <div><a href="http://validator.w3.org/check?uri=referer" title="Valid XHTML" target="_blank"> XHTML</a> <a title="Valid CSS" target="_blank" href="http://jigsaw.w3.org/css-validator/">CSS</a></div>
-         </div>
-        <div id="bottom_right">
-                <div>HTML template by <a href="http://www.templateworld.com/" target="_blank">Template World</a></div><div> <a href="http://designforjoomla.com" target="_blank" title="Joomla template by DesignForJoomla.com">  Joomla template by DesignForJoomla.com</a></div>
-        </div>
-        <br class="clr" />
-</div>
