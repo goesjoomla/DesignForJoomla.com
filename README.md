6297e3a7a6942d686cad9adbc2f438c7962adbd2
Manh-Cuong Nguyen <goesjoomla@gmail.com>
2012-06-14 09:48:42 +0700

    Commit D4J Business 03 to templates/business_03 branch.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-D4J Business 02 template for Joomla! 1.0 series
+D4J Business 03 template for Joomla! 1.0 series
 ===============================================
 
 Install Git client by following the guide at the URL below:
@@ -9,7 +9,7 @@ Then, clone the repository at following URL:
 
 https://github.com/goesjoomla/DesignForJoomla.com.git
 
-Finally, checkout templates/business_02 branch to get D4J Business 02.
+Finally, checkout templates/business_03 branch to get D4J Business 03.
 
 --
 Manh-Cuong Nguyen
--- index.php
+++ index.php
@@ -17,34 +17,42 @@ echo '<?xml version="1.0" encoding="'. $iso[1] .'"?' .'>';
 	<meta name="Publisher" content="Your Mambo Design">
 	<meta name="Language" content="en">
 	<link rel="shortcut icon" href="<?php echo $GLOBALS['mosConfig_live_site'];?>/images/favicon.ico" />
-	<link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['mosConfig_live_site']; ?>/templates/business_02/css/template_css.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['mosConfig_live_site']; ?>/templates/business_03/css/template_css.css" />
+<script language="JavaScript">
+<!--
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+// -->
+</script>
 </head>
 
-<BODY text="#999999" bottomMargin="0" vLink="#ffFFFF" bgColor="#003333" leftMargin="0" topMargin="0" rightMargin="0" MARGINHEIGHT="0" MARGINWIDTH="0" background="templates/business_02/images/page_bg.gif" link="#ffffff">
-
-<TABLE cellSpacing=0 cellPadding=0 border=0 height="120" width="100%">
-  <tr>
-    <td background="templates/business_02/images/header_img.gif" style="background-repeat: no-repeat;" width="291" height="120" nowrap></td>
-    <td width="100%" background="templates/business_02/images/header_bg2.gif" style="background-repeat: repeat-x;">
-<!-- Show Logo, Site's Title & Slogan - Begin -->
-		<table border="0" cellpadding="0" cellspacing="0"><tr>
-			<td width="25%">
+<body bgcolor="#dddddd" text="#000000">
+<table cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" align="center">
+  <tr> 
+    <td width="115" rowspan="4" valign="top" background="templates/business_03/images/LeftTab.jpg" style="background-repeat: repeat-x;">
+<!-- Load Site's Logo - Begin -->
+				<br/>
 				<div align="center"><a href="<?php echo $GLOBALS['mosConfig_live_site']; ?>" title="<?php echo strtoupper($GLOBALS['mosConfig_sitename']); ?>">
-					<IMG border="0" width="89" height="89" align="absmiddle" SRC="<?php echo $GLOBALS['mosConfig_live_site']; ?>/templates/business_02/images/logo.gif" alt="<?php echo $GLOBALS['mosConfig_sitename']; ?> - <?php echo $mosConfig_MetaDesc; ?>"/>
+					<IMG border="0" align="absmiddle" SRC="<?php echo $GLOBALS['mosConfig_live_site']; ?>/templates/business_03/images/logo.gif" alt="<?php echo $GLOBALS['mosConfig_sitename']; ?> - <?php echo $mosConfig_MetaDesc; ?>"/>
 				</a></div>
-			</td>
-			<td width="75%" valign="middle">
-<?php $site_title = explode(' - ', $GLOBALS['mosConfig_sitename']); ?>
-				<font face="Arial" size="3" color="#ffffff"><?php echo strtoupper($site_title[0]); ?></font><br/>
-				<font face="Arial" size="2" color="#c0c0c0"><?php echo $site_title[1]; ?></font>
-			</td>
-		</tr></table>
-<!-- Show Logo, Site's Title & Slogan - End -->
+				<br/>
+<!-- Load Site's Logo - End -->
+      <table width="100%" border="0">
+        <tr> 
+          <td valign="top" style="padding: 3px;">
+<!-- Load Left Module - Begin -->
+	<?php mosLoadModules("left"); ?>
+<!-- Load Left Module - End -->
+          </td>
+        </tr>
+      </table>
+      <p>&nbsp;</p>
     </td>
-  </tr>
-  <tr>
-  	<td background="templates/business_02/images/header_img2.gif" style="background-repeat: no-repeat;" width="291" height="54" nowrap></td>
-    <td width="100%" background="templates/business_02/images/header_bg.gif" style="background-repeat: repeat-x;" align="right" valign="middle">
+    <td width="648" colspan="7" align="center" valign="middle" background="templates/business_03/images/bar1.jpg" style="background-repeat: no-repeat; padding: 2px;">
 <!-- Top Menu - Start -->
 <?php
 	$database->setQuery("SELECT id, name, link FROM #__menu WHERE menutype='mainmenu' and parent='0' AND access<='$gid' AND sublevel='0' AND published='1' ORDER BY ordering");
@@ -64,57 +72,74 @@ echo '<?xml version="1.0" encoding="'. $iso[1] .'"?' .'>';
 <!-- Top Menu - End -->
     </td>
   </tr>
-</TABLE>
-<TABLE cellSpacing=0 cellPadding=0 border=0 height="100%" width="100%">
-  <TR>
-    <TD valign=top> 
-      <table width="100%" border="0" cellspacing="0" cellpadding="0">
-        <tr> 
-          <td width="20%" valign="top" style="border-right: 1px solid #c0c0c0; border-bottom: 1px solid #c0c0c0;">
-			<table width="100%" border="0" cellspacing="0" cellpadding="0">
-			  <tr><td valign="top" style="padding: 5px;">
-<!-- Load Left Module - Begin -->
-	<?php mosLoadModules("left"); ?>
-<!-- Load Left Module - End -->
-			  </td></tr>
-			</table>
-          </td>
-          <td width="80%" valign="top"> 
-			<table width="100%" border="0" cellspacing="0" cellpadding="0">
-			  <tr><td colspan=2 align="center" valign="middle">
-<!-- Load Banner Module - Begin -->
-	<div style="padding: 5px;">
-		<?php if (mosCountModules('banner') > 0) { mosLoadModules( 'banner', -1 ); } ?>
-	</div>
-<!-- Load Banner Module - End -->
-			  </td></tr>
-			  <tr><td valign="top" style="padding: 5px;" width="75%">
+  <tr> 
+    <td width="648" colspan="7" align="center" valign="top">
+<!-- Load Site's Title and Slogan - Begin -->
+<br/>
+<table border="0" cellspacing="0" cellpadding="0" width="100%">
+  <tr>
+    <td width="17" nowrap>&nbsp;</td>
+    <td width="615" height="86" valign="middle" background="templates/business_03/images/Bannert.jpg" style="background-repeat: no-repeat;">
+<?php $site_title = explode(' - ', $GLOBALS['mosConfig_sitename']); ?>
+&nbsp;&nbsp;&nbsp;<font face="Arial" size="4" color="#ff6600"><?php echo strtoupper($site_title[0]); ?></font><br/>
+&nbsp;&nbsp;&nbsp;<font face="Arial" size="3" color="#ff9900"><?php echo $site_title[1]; ?></font>
+    </td>
+    <td width="16" nowrap>&nbsp;</td>
+  </tr>
+</table>
+<br/>
+<!-- Load Site's Title and Slogan - End -->
+    </td>
+  </tr>
+  <tr> 
+    <td width="27" valign="top"></td>
+    <td width="243" colspan="2" valign="top"> 
+      <img src="templates/business_03/images/STL1.jpg" width="243" align="center"><br/><br/>
+<!-- Load Right Module - Begin -->
+		<div align="center" style="background-color: #DEDDED; padding: 1px;"><?php mosLoadModules ( "right" ); ?></div>
+<!-- Load Right Module - End -->
+    </td>
+    <td width="14" valign="top"></td>
+    <td width="333" colspan="2" valign="top"> 
 <!-- Load Top Module, Main Content & Bottom Module - Begin -->
 	<?php if (mosCountModules('top') > 0) { mosLoadModules ( "top" ); echo '<hr/>'; } ?>
 	<?php mosMainBody(); ?>
 	<?php if (mosCountModules('bottom') > 0) { echo '<hr/>'; mosLoadModules ( "bottom" ); } ?>
 <!-- Load Top Module, Main Content & Bottom Module - End -->
-			  </td>
-			  <td valign="top" style="padding: 5px; border-left: 1px solid #c0c0c0; border-top: 1px solid #c0c0c0; border-bottom: 1px solid #c0c0c0;" width="25%">
-<!-- Load Right Module - Begin -->
-	<?php mosLoadModules ( "right" ); ?>
-<!-- Load Right Module - End -->
-			  </td></tr>
-			</table>
-          </td>
-        </tr>
-        <tr><td colspan=2 align="center" valign="middle">
+    </td>
+    <td width="31" valign="top"></td>
+  </tr>
+  <tr> 
+    <td width="27" valign="top"></td>
+    <td width="54" valign="top"></td>
+    <td width="504" colspan="3" valign="top">
+      <img src="templates/business_03/images/bar2.jpg" width="504" align="center">
+<!-- Load Banner Module - Begin -->
+	<div align="center" style="padding: 10px;">
+		<?php if (mosCountModules('banner') > 0) { mosLoadModules( 'banner', -1 ); } ?>
+	</div>
+<!-- Load Banner Module - End -->
+    </td>
+  </tr>
+  <tr height="2">
+    <td width="115" valign="top" background="templates/business_03/images/LeftTab.jpg"></td>
+    <td width="27" valign="top" background="templates/business_03/images/LeftTab.jpg"></td>
+    <td width="54" valign="top" background="templates/business_03/images/LeftTab.jpg"></td>
+    <td width="189" valign="top" background="templates/business_03/images/LeftTab.jpg"></td>
+    <td width="14" valign="top" background="templates/business_03/images/LeftTab.jpg"></td>
+    <td width="301" valign="top" background="templates/business_03/images/LeftTab.jpg"></td>
+    <td width="32" valign="top" background="templates/business_03/images/LeftTab.jpg"></td>
+    <td width="31" valign="top" background="templates/business_03/images/LeftTab.jpg"></td>
+  </tr>
+  <tr>
+  	<td colspan="8" align="center" style="padding: 5px;">
 <!-- Credit Line - Begin -->
-<div style="padding: 5px;">
-	<br/>Template source from <a href="http://freesitetemplates.com" target="_blank">Free Site Templates</a> by <a href="mailto:jim@jimworld.com" target="_blank">JimWORLD</a>. <a href="http://designforjoomla.com" target="_blank" title="Joomla template by DesignForJoomla.com">Joomla template by DesignForJoomla.com</a>.
+Template source from <a href="http://freesitetemplates.com" target="_blank">Free Site Templates</a> by <a href="mailto:jim@jimworld.com" target="_blank">JimWORLD</a>.<a href="http://designforjoomla.com" target="_blank" title="Joomla template by DesignForJoomla.com">Joomla template by DesignForJoomla.com</a>.
+<?php include_once( $GLOBALS['mosConfig_absolute_path'] . '/includes/footer.php' ); ?>
 <!-- Credit Line - End -->
-        </td></tr>
-      </table>
-    </TD>
-  </TR>
-</TABLE>
-
-</BODY>
-
-</HTML>
-
+  	</td>
+  </tr>
+</table>
+</body>
+<!-- /* Joomla Template by DesignForJoomla.com */ -->
+</html>
